4386ec59d210377b9373f2f8ffb22ac5e771c084
NekosAreHot <46061866+NekosAreHot@users.noreply.github.com>
2018-12-22 00:41:27 -0600

    finished level randomizer

--- assets/levels/level.js
+++ assets/levels/level.js
@@ -14,8 +14,15 @@ Game.Level.clear = function(){
 	Game.Text;
 	Game.backBlock = undefined;
 }
+/**
+* Chooses a random level and loads it into memory
+*/
+
 Game.Level.chooseRandom = function(){
     Game.getRandom(Game.Level.levels);
 }
 Game.Level.levels = [];
+Game.Level.level.push(Game.levelOne());
+Game.Level.level.push(Game.levelTwo());
+Game.Level.level.push(Game.levelThree());
 	

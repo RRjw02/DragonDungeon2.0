3bee3fe15aae86f66d47d0f8d8f728af916edfd1
NekosAreHot <46061866+NekosAreHot@users.noreply.github.com>
2018-12-21 23:08:04 -0600

    added documentation

--- assets/entities.js
+++ assets/entities.js
@@ -1,6 +1,10 @@
 Game.Entities = {};
+//All the entities in game excluding the player
 Game.Entities.inGame = [];
-
+/**
+* Checks to see if an gameobject hit an entity
+* @param {object} other - the GameObject you wanna check to see if it hit a entity
+**/
 Game.Entities.checkHit = function(other){
 	for(var i = 0; i < Game.Entities.inGame.length; i++){
 		if(other.crashWith(Game.Entities.inGame[i]) && Game.Entities.inGame[i].isHidden != true){
@@ -9,6 +13,11 @@ Game.Entities.checkHit = function(other){
 	}
 	return false;
 }
+/**
+* Returns an entity at (x,y)
+* @param {int} x - the x position to look for
+* @param {int} y - the y position to look for
+*/
 Game.Entities.getEntity = function(x,y){
 	for(var i = 0; i < Game.Entities.inGame.length; i++){
 		if(Game.Entities.inGame[i].x == x && Game.Entities.inGame[i].y == y){
@@ -17,27 +26,12 @@ Game.Entities.getEntity = function(x,y){
 	}
 		return false;
 }
-//Broken
-Game.Entities.createRandomEntity = function(x,y){
-	var x = Game.getRandom(Game.Entities.list);
-	if(x == 1){
-		Game.Entities.createBull(x,y,5);
-	}else if(x == 2){
-		Game.Entities.createBat(x,y,5);
-	}else if(x == 3){
-		Game.Entities.createMimic(x,y,5);
-	}else if(x == 4){
-		Game.Entities.createGoblin(x,y,5);
-	}
-//	Game.Entities.inGame[Game.Entities.length - 1].x = x;
-//	Game.Entities.inGame[Game.Entities.length - 1].y = y;
-}
-Game.Entities.list = {
-	bull : 1,
-	bat : 2,
-	mimic : 3,
-	goblin : 4
-}
+/**
+* Creates a bull entity at (x,y) with health
+* @param {int} x - x position of the bull
+* @param {int} y - y position of the bull
+* @param {int} health - the startinh health of the bull
+*/
 Game.Entities.createBull = function(x,y,health){
 	var temp = new Game.GameObject(20,20,"red",0,0,"entity",true,"Bull");
 	temp.x = x;
@@ -46,7 +40,12 @@ Game.Entities.createBull = function(x,y,health){
 	temp.health = health;
 	Game.Entities.inGame.push(temp);
 }
-
+/**
+* Creates a bat entity at (x,y) with health
+* @param {int} x - x position of the bat
+* @param {int} y - y position of the bat
+* @param {int} health - the startinh health of the bat
+*/
 Game.Entities.createBat = function(x,y,health){
 	var temp = new Game.GameObject(20,20,"brown",0,0,"entity",true,"Bat");
 	temp.x = x;
@@ -55,7 +54,12 @@ Game.Entities.createBat = function(x,y,health){
 	temp.health = health;
 	Game.Entities.inGame.push(temp);
 }
-
+/**
+* Creates a mimic entity at (x,y) with health
+* @param {int} x - x position of the mimic
+* @param {int} y - y position of the mimic
+* @param {int} health - the startinh health of the mimic
+*/
 Game.Entities.createMimic = function(x,y,health){
 	var temp = new Game.GameObject(20,20,Game.File.getImageDirectory() + "chest_texture.png",0,0,"entity",true,"Mimic");
 	temp.x = x;
@@ -64,6 +68,12 @@ Game.Entities.createMimic = function(x,y,health){
 	temp.health = health;
 	Game.Entities.inGame.push(temp);
 }
+/**
+* Creates a goblin entity at (x,y) with health
+* @param {int} x - x position of the goblin
+* @param {int} y - y position of the goblin
+* @param {int} health - the startinh health of the goblin
+*/
 Game.Entities.createGoblin = function(x,y,health){
 	var temp = new Game.GameObject(20,20,Game.File.getImageDirectory() + "goblin_texture.png",0,0,"entity", true, "Goblin");
 	temp.x = x;

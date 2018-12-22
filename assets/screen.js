2f0d50c13baebd2d7228bae1ceffadadb7b37786
NekosAreHot <46061866+NekosAreHot@users.noreply.github.com>
2018-12-21 22:36:09 -0600

    added some screen functions

--- assets/screen.js
+++ assets/screen.js
@@ -1,7 +1,10 @@
 Game.Screen = {};
 Game.Screen.getWidth = function(){
-
+     return 1200;
 }
 Game.Screen.getHeight = function(){
-
+     return 700;
+}
+Game.Screen.rezise = function(width,height){
+  window.resizeTo(width,height);
 }

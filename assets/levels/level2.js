f3eaec0fb24374534440785df51c74b928c19593
NekosAreHot <46061866+NekosAreHot@users.noreply.github.com>
2018-12-22 12:33:38 -0600

    finished, jist needs to fill in the space outside of the area with dirt

--- assets/levels/level2.js
+++ assets/levels/level2.js
@@ -3,8 +3,8 @@ Game.levelTwo = function(){
 	Game.Demo();
 	
 	//Player is 20pixels by 20pixles at (x,y) (0,120)
-	Game.player.x = 60;
-	Game.player.y = 40;
+	Game.Teleport(60,40);
+	
 	Game.player.facing = "east";
 	//BackGround
 	Game.createBackgroundPrefix();

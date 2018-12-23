2fd3fde1c79f5fda9f91a7a3c893e95007fbb87f
NekosAreHot <46061866+NekosAreHot@users.noreply.github.com>
2018-12-22 21:18:43 -0600

    Update player.js

--- assets/player.js
+++ assets/player.js
@@ -35,3 +35,29 @@ Game.Player.inventoryRefresh = function(){
 Game.getPlayerCoords = function(){
 	return "X: " + Game.player.x + " Y: " + Game.player.y;
 }
+/**
+* Teleports the player to x,y
+* @param {int} x - the new x position
+* @param {int} y - the new y position
+*/
+Game.Teleport = function(x,y){
+        Game.player.x = x;
+	Game.player.y = y;
+	
+}
+/**
+* Sets the players Texture
+* @param {string} filename - the filename in assets/images
+*/
+Game.setPlayerSkin = function(filename){
+     Game.player.image.src = Game.File.getImageDirectory() + filename;
+}
+/**
+* Returns String of the current image the player is
+*/
+Game.getPlayerSkin = function(){
+     return Game.player.image.src;
+}
+	
+	
+	
